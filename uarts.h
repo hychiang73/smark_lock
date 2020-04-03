5b53b8c5d9bedaf880893cb595f0d1e2507fe329
hychiang <dickychiang73@gmail.com>
2020-04-03 16:38:06 +0100

    Change proper description

--- uarts.h
+++ uarts.h
@@ -1,14 +1,27 @@
-/****************************************Copyright (c)************************************************
-**                                      [艾克姆科技]
-**                                        IIKMSIK 
-**                            官方店铺：https://acmemcu.taobao.com
-**                            官方论坛：http://www.e930bbs.com                                 
-**--------------File Info-----------------------------------------------------------------------------
-** File          name:my_ble_uarts.h
-** Last modified Date:          
-** Last       Version:		   
-** Descriptions      :串口透传服务头文件			
-**---------------------------------------------------------------------------------------------------*/
+/**@file
+ *
+ * @defgroup ble_nus Nordic UART Service
+ * @{
+ * @ingroup  ble_sdk_srv
+ * @brief    Nordic UART Service implementation.
+ *
+ * @details The Nordic UART Service is a simple GATT-based service with TX and RX characteristics.
+ *          Data received from the peer is passed to the application, and the data received
+ *          from the application of this service is sent to the peer as Handle Value
+ *          Notifications. This module demonstrates how to implement a custom GATT-based
+ *          service and characteristics using the SoftDevice. The service
+ *          is used by the application to send and receive ASCII text strings to and from the
+ *          peer.
+ *
+ * @note    The application must register this module as BLE event observer using the
+ *          NRF_SDH_BLE_OBSERVER macro. Example:
+ *          @code
+ *              ble_nus_t instance;
+ *              NRF_SDH_BLE_OBSERVER(anything, BLE_NUS_BLE_OBSERVER_PRIO,
+ *                                   ble_nus_on_ble_evt, &instance);
+ *          @endcode
+ */
+
 #ifndef BLE_MY_UARTS_H__
 #define BLE_MY_UARTS_H__
 #include <stdint.h>
@@ -23,14 +36,17 @@
 extern "C" {
 #endif
 
-//定义串口透传服务实例，该实例完成2件事情
-//1：定义了static类型串口透传服务结构体变量，为串口透传服务结构体分配了内存
-//2：注册了BLE事件监视者，这使得串口透传程序模块可以接收BLE协议栈的事件，从而可以在ble_uarts_on_ble_evt()事件回调函数中处理自己感兴趣的事件
-#define BLE_UARTS_DEF(_name, _uarts_max_clients)                                      \
+/**@brief   Macro for defining a ble_nus instance.
+ *
+ * @param     _name            Name of the instance.
+ * @param[in] _nus_max_clients Maximum number of NUS clients connected at a time.
+ * @hideinitializer
+ */
+#define BLE_UARTS_DEF(_name, _uarts_max_clients)                  \
     BLE_LINK_CTX_MANAGER_DEF(CONCAT_2(_name, _link_ctx_storage),  \
-                             (_uarts_max_clients),                  \
-                             sizeof(ble_uarts_client_context_t));   \
-	  static ble_uarts_t _name =                                     \
+                             (_uarts_max_clients),                \
+                             sizeof(ble_uarts_client_context_t)); \
+	  static ble_uarts_t _name =                                    \
 	  {                                                             \
         .p_link_ctx_storage = &CONCAT_2(_name, _link_ctx_storage) \
     };                                                            \
@@ -39,60 +55,17 @@ extern "C" {
                          ble_uarts_on_ble_evt,                    \
                          &_name)
 
-//定义串口透传服务128位UUID基数
+// 128bis UUID
 #define UARTS_BASE_UUID      {{0x40, 0xE3, 0x4A, 0x1D, 0xC2, 0x5F, 0xB0, 0x9C, 0xB7, 0x47, 0xE6, 0x43, 0x00, 0x00, 0x53, 0x86}} 
-//定义服务和特征的16位UUID
-#define BLE_UUID_UARTS_SERVICE 0x000A              //串口透传服务16位UUID
-#define BLE_UUID_UARTS_TX_CHARACTERISTIC 0x000B    //TX特征16位UUID           
-#define BLE_UUID_UARTS_RX_CHARACTERISTIC 0x000C    //RX特征16位UUID
+// 16bit UUID of characteristic and service
+#define BLE_UUID_UARTS_SERVICE 0x000A
+#define BLE_UUID_UARTS_TX_CHARACTERISTIC 0x000B           
+#define BLE_UUID_UARTS_RX_CHARACTERISTIC 0x000C
 
-//定义串口透传事件类型，这是用户自己定义的，供应用程序使用
-typedef enum
-{
-    BLE_UARTS_EVT_RX_DATA,      //接收到新的数据
-    BLE_UARTS_EVT_TX_RDY,       //准备就绪，可以发送新数据
-	  BLE_NUS_EVT_COMM_STARTED,   //通知已经使能
-    BLE_NUS_EVT_COMM_STOPPED,   //通知已经禁止
-} ble_uarts_evt_type_t;	
-	
-/* Forward declaration of the ble_nus_t type. */
-typedef struct ble_uarts_s ble_uarts_t;
-
-
-//串口透传服务BLE_NUS_EVT_RX_DATA事件数据结构体，该结构体用于当BLE_NUS_EVT_RX_DATA产生时将接收的数据信息传递给处理函数
-typedef struct
-{
-    uint8_t const * p_data; //指向存放接收数据的缓存
-    uint16_t        length; //接收的数据长度
-} ble_uarts_evt_rx_data_t;
-
-//记录对端设备是否使能了RX特征的通知
-typedef struct
-{
-    bool is_notification_enabled; 
-} ble_uarts_client_context_t;
-
-//串口透传服务事件结构体
-typedef struct
-{
-    ble_uarts_evt_type_t       type;        //事件类型
-    ble_uarts_t                * p_uarts;   //指向串口透传实例的指针
-    uint16_t                   conn_handle; //连接句柄
-    ble_uarts_client_context_t * p_link_ctx;//指向link context
-    union
-    {
-        ble_uarts_evt_rx_data_t rx_data; //BLE_NUS_EVT_RX_DATA事件数据
-    } params;
-} ble_uarts_evt_t;
-
-
-
-//定义操作码长度
 #define OPCODE_LENGTH        1
-//定义句柄长度
 #define HANDLE_LENGTH        2
 
-//定义最大传输数据长度（字节数）
+/**@brief   Maximum length of data (in bytes) that can be transmitted to the peer by the Nordic UART service module. */
 #if defined(NRF_SDH_BLE_GATT_MAX_MTU_SIZE) && (NRF_SDH_BLE_GATT_MAX_MTU_SIZE != 0)
     #define BLE_UARTS_MAX_DATA_LEN (NRF_SDH_BLE_GATT_MAX_MTU_SIZE - OPCODE_LENGTH - HANDLE_LENGTH)
 #else
@@ -100,38 +73,127 @@ typedef struct
     #warning NRF_SDH_BLE_GATT_MAX_MTU_SIZE is not defined.
 #endif
 
-//定义函数指针类型ble_uarts_data_handler_t
+
+/**@brief   Nordic UART Service event types. */
+typedef enum
+{
+    BLE_UARTS_EVT_RX_DATA,      /**< Data received. */
+    BLE_UARTS_EVT_TX_RDY,       /**< Service is ready to accept new data to be transmitted. */
+	  BLE_NUS_EVT_COMM_STARTED,   /**< Notification has been enabled. */
+    BLE_NUS_EVT_COMM_STOPPED,   /**< Notification has been disabled. */
+} ble_uarts_evt_type_t;	
+	
+/* Forward declaration of the ble_nus_t type. */
+typedef struct ble_uarts_s ble_uarts_t;
+
+
+/**@brief   Nordic UART Service @ref BLE_NUS_EVT_RX_DATA event data.
+ *
+ * @details This structure is passed to an event when @ref BLE_NUS_EVT_RX_DATA occurs.
+ */
+typedef struct
+{
+    uint8_t const * p_data; /**< A pointer to the buffer with received data. */
+    uint16_t        length; /**< Length of received data. */
+} ble_uarts_evt_rx_data_t;
+
+
+/**< Variable to indicate if the peer has enabled notification of the RX characteristic.*/
+typedef struct
+{
+    bool is_notification_enabled; 
+} ble_uarts_client_context_t;
+
+
+/**@brief   Nordic UART Service event structure.
+ *
+ * @details This structure is passed to an event coming from service.
+ */
+typedef struct
+{
+    ble_uarts_evt_type_t       type;        /**< Event type. */
+    ble_uarts_t                * p_uarts;   /**< A pointer to the instance. */
+    uint16_t                   conn_handle; /**< Connection handle. */
+    ble_uarts_client_context_t * p_link_ctx;/**< A pointer to the link context. */
+    union
+    {
+        ble_uarts_evt_rx_data_t rx_data; /**< @ref BLE_NUS_EVT_RX_DATA event data. */
+    } params;
+} ble_uarts_evt_t;
+
+
+/**@brief Nordic UART Service event handler type. */
 typedef void (* ble_uarts_data_handler_t) (ble_uarts_evt_t * p_evt);
 
 
-//串口服务初始化结构体
+/**@brief   Nordic UART Service initialization structure.
+ *
+ * @details This structure contains the initialization information for the service. The application
+ * must fill this structure and pass it to the service using the @ref ble_nus_init
+ *          function.
+ */
 typedef struct
 {
-    ble_uarts_data_handler_t data_handler; //处理接收数据的事件句柄
+    ble_uarts_data_handler_t data_handler; /**< Event handler to be called for handling received data. */
 } ble_uarts_init_t;
 
 
-//串口透传服务结构体，包含所需要的信息
+/**@brief   Nordic UART Service structure.
+ *
+ * @details This structure contains status information related to the service.
+ */
 struct ble_uarts_s
 {
-    uint8_t                         uuid_type;          //UUID类型
-    uint16_t                        service_handle;     //串口透传服务句柄（由协议栈提供）
-    ble_gatts_char_handles_t        tx_handles;         //TX特征句柄
-    ble_gatts_char_handles_t        rx_handles;         //RX特征句柄
-	  blcm_link_ctx_storage_t * const p_link_ctx_storage; //指向存储所有当前连接及其context的“link context”的句柄的指针
-    ble_uarts_data_handler_t        data_handler;       //处理接收数据的事件句柄
+    uint8_t                         uuid_type;          /**< UUID type for Nordic UART Service Base UUID. */
+    uint16_t                        service_handle;     /**< Handle of Nordic UART Service (as provided by the SoftDevice). */
+    ble_gatts_char_handles_t        tx_handles;         /**< Handles related to the TX characteristic (as provided by the SoftDevice). */
+    ble_gatts_char_handles_t        rx_handles;         /**< Handles related to the RX characteristic (as provided by the SoftDevice). */
+	  blcm_link_ctx_storage_t * const p_link_ctx_storage; /**< Pointer to link context storage with handles of all current connections and its context. */
+    ble_uarts_data_handler_t        data_handler;       /**< Event handler to be called for handling received data. */
 };
 
-
-
-
-void ble_uarts_on_ble_evt(ble_evt_t const * p_ble_evt, void * p_context);
+/**@brief   Function for initializing the Nordic UART Service.
+ *
+ * @param[out] p_nus      Nordic UART Service structure. This structure must be supplied
+ *                        by the application. It is initialized by this function and will
+ *                        later be used to identify this particular service instance.
+ * @param[in] p_nus_init  Information needed to initialize the service.
+ *
+ * @retval NRF_SUCCESS If the service was successfully initialized. Otherwise, an error code is returned.
+ * @retval NRF_ERROR_NULL If either of the pointers p_nus or p_nus_init is NULL.
+ */
 uint32_t ble_uarts_init(ble_uarts_t * p_uarts, ble_uarts_init_t const * p_uarts_init);
+
+
+/**@brief   Function for handling the Nordic UART Service's BLE events.
+ *
+ * @details The Nordic UART Service expects the application to call this function each time an
+ * event is received from the SoftDevice. This function processes the event if it
+ * is relevant and calls the Nordic UART Service event handler of the
+ * application if necessary.
+ *
+ * @param[in] p_ble_evt     Event received from the SoftDevice.
+ * @param[in] p_context     Nordic UART Service structure.
+ */
+void ble_uarts_on_ble_evt(ble_evt_t const * p_ble_evt, void * p_context);
+
+
+/**@brief   Function for sending a data to the peer.
+ *
+ * @details This function sends the input string as an RX characteristic notification to the
+ *          peer.
+ *
+ * @param[in]     p_nus       Pointer to the Nordic UART Service structure.
+ * @param[in]     p_data      String to be sent.
+ * @param[in,out] p_length    Pointer Length of the string. Amount of sent bytes.
+ * @param[in]     conn_handle Connection Handle of the destination client.
+ *
+ * @retval NRF_SUCCESS If the string was sent successfully. Otherwise, an error code is returned.
+ */
 uint32_t ble_uarts_data_send(ble_uarts_t * p_uarts,
                            uint8_t   * p_data,
                            uint16_t  * p_length,
                            uint16_t    conn_handle);
-
 
 
 #ifdef __cplusplus
